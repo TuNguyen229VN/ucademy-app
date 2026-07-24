fa5c4bd18716933372f72fce1f5faf96e1cafd55
TuNguyen229VN <insec229@gmail.com>
2026-07-24 16:59:25 +0700

    feat: create course ui

---
+++ components/courses/CourseAddNew.tsx
@@ -0,0 +1,103 @@
+"use client";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Controller, useForm } from "react-hook-form";
+import * as z from "zod";
+import { Input } from "../ui/input";
+import { Field, FieldError, FieldGroup, FieldLabel } from "../ui/field";
+import { Button } from "../ui/button";
+import { useState } from "react";
+import slugify from "slugify";
+
+const formSchema = z.object({
+  title: z.string().min(10, "Tên khóa học phải có ít nhất 10 ký tự"),
+  slug: z.string().optional(),
+});
+
+function CourseAddNew() {
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: "",
+      slug: "",
+    },
+    mode: "onSubmit",
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    setIsSubmitting(true);
+    try {
+      const data = {
+        title: values.title,
+        slug:
+          values.slug ||
+          slugify(values.title, {
+            lower: true,
+            locale: "vi",
+          }),
+      };
+      console.log(data);
+    } catch (error) {
+    } finally {
+      setIsSubmitting(false);
+    }
+  }
+
+  return (
+    <form
+      id="form-rhf-demo"
+      onSubmit={form.handleSubmit(onSubmit)}
+      autoComplete="off"
+    >
+      <FieldGroup className="grid grid-cols-2 gap-8 mt-10 mb-6">
+        <Controller
+          name="title"
+          control={form.control}
+          render={({ field, fieldState }) => (
+            <Field data-invalid={fieldState.invalid}>
+              <FieldLabel htmlFor="form-rhf-title">Tên khóa học *</FieldLabel>
+              <Input
+                {...field}
+                id="form-rhf-title"
+                aria-invalid={fieldState.invalid}
+                placeholder="Tên khóa học"
+                autoComplete="off"
+              />
+              {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+            </Field>
+          )}
+        />
+        <Controller
+          name="slug"
+          control={form.control}
+          render={({ field, fieldState }) => (
+            <Field data-invalid={fieldState.invalid}>
+              <FieldLabel htmlFor="form-rhf-slug">
+                Đường dẫn khóa học
+              </FieldLabel>
+              <Input
+                {...field}
+                id="form-rhf-slug"
+                placeholder="khoa-hoc-lap-trinh"
+                aria-invalid={fieldState.invalid}
+              />
+              {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+            </Field>
+          )}
+        />
+      </FieldGroup>
+      <Button
+        isLoading={isSubmitting}
+        type="submit"
+        form="form-rhf-demo"
+        className="w-30 dark:text-white"
+        disabled={isSubmitting}
+      >
+        Tạo khóa học
+      </Button>
+    </form>
+  );
+}
+
+export default CourseAddNew;
--- components/typography/Heading.tsx
+++ components/typography/Heading.tsx
@@ -1,7 +1,7 @@
 import { ReactNode } from "react";
 
 const Heading = ({ children }: { children: ReactNode }) => {
-  return <h1 className="text-3xl font-bold">{children}</h1>;
+  return <h1 className="text-2xl md:text-3xl font-bold">{children}</h1>;
 };
 
 export default Heading;
