3b2b8dedd598cb66026caae962079433909702b3
TuNguyen229VN <insec229@gmail.com>
2026-07-28 22:46:12 +0700

    feat: detail course landing ui

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -3,7 +3,7 @@ import { ReactNode } from "react";
 
 const DashboardLayout = ({ children }: { children: ReactNode }) => {
   return (
-    <div className="wrapper grid grid-cols-[300px_minmax(0,1fr)] h-screen">
+    <div className="wrapper grid lg:grid-cols-[300px_minmax(0,1fr)] h-screen">
       <Sidebar />
       <main className="p-5">{children}</main>
     </div>
